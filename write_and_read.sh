eee5c4a469c074bb6befee8e73151d7d9d592613
theCaptN21 <k.hen.0246@gmail.com>
2025-02-10 21:00:57 +0000

    Small modifications

--- write_and_read.sh
+++ write_and_read.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Write data to the volume
-echo "Hello from the Docker volume!" > /data/volume_test.txt
+echo "Hello from the Docker volume!" > /data/volume_test1.txt
 
 # Read back the data
-cat /data/volume_test.txt
+cat /data/volume_test1.txt
 
 # Keep the container running
 tail -f /dev/null
